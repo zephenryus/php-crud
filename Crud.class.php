5e7f8519c0636dffef43669409f59b50870ab781
Kevin Jensen <zephenryus@gmail.com>
2014-05-21 08:46:45 -0600

    Update Crud.class.php
    Added long description to comments

--- Crud.class.php
+++ Crud.class.php
@@ -3,7 +3,12 @@
 /**
  * Create-Read-Update-Delete Package
  *
- * Long description for file (if any)...
+ * A PHP framework which offers easy access to create, read, update and delete
+ * records of a MySQL database. The purpose of this class is to create a public
+ * interface with a MySQL database which is more intuitive and PHP programming
+ * centric. Rather than submitting SQL queries to the database, associative arrays
+ * containing values are passed to the methods which are then parsed into create,
+ * read, update and delete queries.
  *
  * PHP version 5
  *
